90525336ab12a91483dc2794b6f5952c25f6e714
delinuxist <delinuxist@gmail.com>
2022-06-11 00:35:40 +0000

    imported all components

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,25 @@
 import Image from "next/image";
+import Head from "next/head";
 import BackgroundAnimation from "../components/BgAnimation/BgAnimation";
+import Navbar from "../components/layouts/Navbar";
+import Hero from "../components/Hero";
+import About from "../components/About";
+import Skills from "../components/Skills";
+import Project from "../components/Project";
 
 export default function Home() {
-  return <div className=" bg-slate-900">Initial setup</div>;
+  return (
+    <>
+      <Head>
+        <title>Emmanuel | Software Engineer</title>
+      </Head>
+      <div>
+        <Navbar />
+        <Hero />
+        <About />
+        <Skills />
+        <Project />
+      </div>
+    </>
+  );
 }
