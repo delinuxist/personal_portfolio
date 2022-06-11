65c5f1c0d2f4022d9db97072d1ab874d9ad829bf
delinuxist <delinuxist@gmail.com>
2022-06-11 01:02:50 +0000

    imported all png to replace png relative paths

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -1,6 +1,17 @@
-import Image from "next/image";
 import React from "react";
 import SkillsPng from "./SkillsItem";
+import mongo from "../public/assets/img/mongo.png";
+import react from "../public/assets/img/react.png";
+import tailwind from "../public/assets/img/tailwind.png";
+import github from "../public/assets/img/github1.png";
+import mysql from "../public/assets/img/mysql.png";
+import nextjs from "../public/assets/img/next.png";
+import css from "../public/assets/img/css.png";
+import html from "../public/assets/img/html.png";
+import grapql from "../public/assets/img/graphql.png";
+import apollo from "../public/assets/img/apollo.png";
+import node from "../public/assets/img/node.png";
+import javascript from "../public/assets/img/javascript.png";
 
 const Skills = () => {
   return (
@@ -11,39 +22,18 @@ const Skills = () => {
         </p>
         <h2 className=" py-4 text-orange-300">What I Can Do</h2>
         <div className="grid md:grid-cols-2 py-4 lg:grid-cols-4 gap-8">
-          <SkillsPng
-            imageUrl="/../public/assets/img/react.png"
-            title="Reactjs"
-          />
-          <SkillsPng
-            imageUrl="/../public/assets/img/tailwind.png"
-            title="Tailwind Css"
-          />
-          <SkillsPng imageUrl="/../public/assets/img/css.png" title="Css" />
-          <SkillsPng
-            imageUrl="/../public/assets/img/mongo.png"
-            title="MongoDB"
-          />
-          <SkillsPng imageUrl="/../public/assets/img/html.png" title="Html" />
-          <SkillsPng imageUrl="/../public/assets/img/next.png" title="Nextjs" />
-          <SkillsPng
-            imageUrl="/../public/assets/img/graphql.png"
-            title="Graphql"
-          />
-          <SkillsPng imageUrl="/../public/assets/img/node.png" title="Nodejs" />
-          <SkillsPng imageUrl="/../public/assets/img/mysql.png" title="Mysql" />
-          <SkillsPng
-            imageUrl="/../public/assets/img/javascript.png"
-            title="Javascript"
-          />
-          <SkillsPng
-            imageUrl="/../public/assets/img/apollo.png"
-            title="Apollo"
-          />
-          <SkillsPng
-            imageUrl="/../public/assets/img/github1.png"
-            title="Github"
-          />
+          <SkillsPng imageUrl={react} title="Reactjs" />
+          <SkillsPng imageUrl={tailwind} title="Tailwind Css" />
+          <SkillsPng imageUrl={css} title="Css" />
+          <SkillsPng imageUrl={mongo} title="MongoDB" />
+          <SkillsPng imageUrl={html} title="Html" />
+          <SkillsPng imageUrl={nextjs} title="Nextjs" />
+          <SkillsPng imageUrl={grapql} title="Graphql" />
+          <SkillsPng imageUrl={node} title="Nodejs" />
+          <SkillsPng imageUrl={mysql} title="Mysql" />
+          <SkillsPng imageUrl={javascript} title="Javascript" />
+          <SkillsPng imageUrl={apollo} title="Apollo" />
+          <SkillsPng imageUrl={github} title="Github" />
         </div>
       </div>
     </div>
